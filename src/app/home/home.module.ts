1c941db2dc1c4e958d2dcc74c512d69457791cae
DPB-Bo <baobo1506@gmail.com>
2023-02-23 17:20:37 +0700

    BaoDP update login component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [HeaderComponent, FooterComponent, HomeComponent, LoginComponent],
   exports: [
     HeaderComponent,
     FooterComponent
